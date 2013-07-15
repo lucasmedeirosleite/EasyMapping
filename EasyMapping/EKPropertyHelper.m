12c5507e5bd0a7a67d349ba1a1e2fea90ce4ffe1
Tony Spinelli <tonyspin@gmail.com>
2013-07-15 09:43:22 -0400

    Removed log statement

--- EasyMapping/EKPropertyHelper.m
+++ EasyMapping/EKPropertyHelper.m
@@ -96,7 +96,7 @@
 
 static const char * getPropertyType(objc_property_t property) {
     const char *attributes = property_getAttributes(property);
-    printf("attributes=%s\n", attributes);
+//    printf("attributes=%s\n", attributes);
     char buffer[1 + strlen(attributes)];
     strcpy(buffer, attributes);
     char *state = buffer, *attribute;
