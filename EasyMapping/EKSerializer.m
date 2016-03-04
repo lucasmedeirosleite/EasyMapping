33551db6bddac257bfee87ee4cf9144fd722566f
ArtemShvydky <smolinouran@gmail.com>
2016-03-04 19:14:55 +0200

    Update EKSerializer.m

--- EasyMapping/EKSerializer.m
+++ EasyMapping/EKSerializer.m
@@ -144,7 +144,7 @@
 +(void)setValueOnRepresentation:(NSMutableDictionary *)representation fromManagedObject:(id)object
             withPropertyMapping:(EKPropertyMapping *)propertyMapping inContext:(NSManagedObjectContext *)context
 {
-    id returnedValue = [object valueForKey:propertyMapping.property];
+    id returnedValue = [object valueForKeyPath:propertyMapping.property];
 
     if (returnedValue) {
 
