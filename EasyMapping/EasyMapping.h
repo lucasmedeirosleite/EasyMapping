b52e4d17386a66711864d81a7f4eacac3c2adc72
Denys Telezhkin <strangervir@gmail.com>
2016-06-30 22:57:46 +0300

    use angular brackets in umbrella header for EasyMapping to avoid Include of non-modular header inside framework warning.

--- EasyMapping/EasyMapping.h
+++ EasyMapping/EasyMapping.h
@@ -21,14 +21,15 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 
-#import "EKMapper.h"
-#import "EKSerializer.h"
-#import "EKObjectMapping.h"
-#import "EKManagedObjectMapper.h"
-#import "EKManagedObjectMapping.h"
-#import "EKMappingBlocks.h"
-#import "EKObjectModel.h"
-#import "EKManagedObjectModel.h"
-#import "EKMappingProtocol.h"
-#import "EKPropertyMapping.h"
-#import "NSDateFormatter+EasyMappingAdditions.h"
+#import <EasyMapping/EKMapper.h>
+#import <EasyMapping/EKSerializer.h>
+#import <EasyMapping/EKObjectMapping.h>
+#import <EasyMapping/EKManagedObjectMapper.h>
+#import <EasyMapping/EKManagedObjectMapping.h>
+#import <EasyMapping/EKMappingBlocks.h>
+#import <EasyMapping/EKObjectModel.h>
+#import <EasyMapping/EKManagedObjectModel.h>
+#import <EasyMapping/EKMappingProtocol.h>
+#import <EasyMapping/EKPropertyMapping.h>
+#import <EasyMapping/EKRelationshipMapping.h>
+#import <EasyMapping/NSDateFormatter+EasyMappingAdditions.h>
